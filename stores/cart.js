27d1452f075f384dec0e2ce3cb5d58cbe40dcb77
pigbao <3210074200@qq.com>
2024-05-08 02:26:55 +0800

    feat: 添加注释

--- stores/cart.js
+++ stores/cart.js
@@ -9,17 +9,20 @@ import {
 
 const isTakeoutStore = useIsTakeoutStore();
 
+// 购物车
 export const useCartStore = defineStore('cart', () => {
+  // 购物车列表
   const cartList = ref([]);
+  // 加入购物车
   async function addCart(data) {
     try {
-      await apiAddCart({...data,isTakeout:isTakeoutStore.isTakeout});
+      await apiAddCart({ ...data, isTakeout: isTakeoutStore.isTakeout });
       getList();
     } catch (error) {
       console.error(error);
     }
   }
-
+  // 更新购物车
   async function updateCart(data) {
     try {
       await apiUpdateCart(data);
@@ -30,6 +33,7 @@ export const useCartStore = defineStore('cart', () => {
     }
   }
 
+  // 删除购物车
   async function delCart(id) {
     try {
       await apiDelCart(id);
@@ -39,7 +43,7 @@ export const useCartStore = defineStore('cart', () => {
       console.error(error);
     }
   }
-
+  // 清空购物车
   async function clearCart() {
     try {
       await apiClearCart(isTakeoutStore.isTakeout);
@@ -48,7 +52,7 @@ export const useCartStore = defineStore('cart', () => {
       console.error(error);
     }
   }
-
+  // 获取购物车列表
   async function getList() {
     try {
       cartList.value = await apiGetCartList(isTakeoutStore.isTakeout);
@@ -56,13 +60,14 @@ export const useCartStore = defineStore('cart', () => {
       console.error(error);
     }
   }
-
+  // 购物车总数量
   const count = computed(() => {
     return cartList.value.reduce((total, item) => {
       return total + item.count;
     }, 0);
   });
 
+  // 购物车总价
   const totalPrice = computed(() => {
     const res = cartList.value.reduce((total, item) => {
       return total + item.count * item.price;
@@ -70,9 +75,13 @@ export const useCartStore = defineStore('cart', () => {
     return res / 100;
   });
 
-  watch(() => isTakeoutStore.isTakeout, () => {
-    getList();
-  });
+  // 监听自取外卖 每次变化 获取一遍购物车列表
+  watch(
+    () => isTakeoutStore.isTakeout,
+    () => {
+      getList();
+    }
+  );
 
   return {
     cartList,
